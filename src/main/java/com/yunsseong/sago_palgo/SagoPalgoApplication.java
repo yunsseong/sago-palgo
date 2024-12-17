c168a0f49c42b94fb202bfe59156f4153894af12
yunsseong <aster20@hanyang.ac.kr>
2024-12-17 22:25:23 +0900

    Init project

---
+++ src/main/java/com/yunsseong/sago_palgo/SagoPalgoApplication.java
@@ -0,0 +1,13 @@
+package com.yunsseong.sago_palgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagoPalgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagoPalgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yunsseong/sago_palgo/SagoPalgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunsseong.sago_palgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagoPalgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
